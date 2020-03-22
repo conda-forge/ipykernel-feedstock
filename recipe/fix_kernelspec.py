c2bda35c04cee3e4a369c29f82c3535aaf5ae4af
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-03-22 11:28:27 -0400

    fix path calls

--- recipe/fix_kernelspec.py
+++ recipe/fix_kernelspec.py
@@ -10,12 +10,17 @@ posix_exe = Path(sys.executable).as_posix()
 
 print("Rewriting kernelspec at:\n\t{}".format(spec_path))
 
-spec = json.loads(spec.spec_path())
+raw_spec = spec_path.read_text()
+
+print(raw_spec)
+
+spec = json.loads(raw_spec)
 
 print("Kernel python was:\n\t{}".format(spec["argv"][0]))
 
-spec["argv"][0] = posix_exe
-
-print("Rewriting kernel python with:\n\t{}".format(posix_exe))
-
-spec_path.write_text(json.dumps(spec, indent=2))
+if spec["argv"][0] == posix_exe:
+    print("Path is fine")
+else:
+    print("Rewriting kernel python with:\n\t{}".format(posix_exe))
+    spec["argv"][0] = posix_exe
+    spec_path.write_text(json.dumps(spec, indent=2))
