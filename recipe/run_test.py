fc14970cb299531232a4a7d4d97e3dea9328fb97
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-10-25 08:25:52 -0500

    update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,81 +2,101 @@ import json
 import os
 import platform
 import sys
-import pkgutil
 import pytest
+import typing
+from pathlib import Path
+
+# TODO: investigate upstream interrupt regression in 6.5.0
+test_skips = ["flaky", "interrupt"]
 
 py_major = sys.version_info[0]
 py_impl = platform.python_implementation().lower()
 machine = platform.machine().lower()
 
+is_aarch = "aarch64" in machine
+is_ppc = "ppc" in machine
+is_pypy = py_impl == "pypy"
 
-print("Python implementation:", py_impl)
-print("              Machine:", machine)
+prefix = Path(os.environ["PREFIX"])
 
-specfile = os.path.join(
-    os.environ["PREFIX"],
-    "share",
-    "jupyter",
-    "kernels",
-    "python{}".format(py_major),
-    "kernel.json",
-)
 
-print("Checking Kernelspec at:     ", specfile, "...\n")
+def check_kernel() -> int:
+    print(f"Python implementation: {py_impl}")
+    print(f"              Machine: {machine}")
 
-with open(specfile, "r") as fh:
-    raw_spec = fh.read()
+    specfile = prefix / f"share/jupyter/kernels/python{py_major}/kernel.json"
 
-print(raw_spec)
+    print(f"Checking Kernelspec at:     {specfile}...")
 
-spec = json.loads(raw_spec)
+    raw_spec = specfile.read_text(encoding="utf-8")
 
-print("\nChecking python executable", spec["argv"][0], "...")
+    print(raw_spec)
 
-if spec["argv"][0].replace("\\", "/") != sys.executable.replace("\\", "/"):
-    print(
-        "The kernelspec seems to have the wrong prefix. \n"
-        "Specfile: {}\n"
-        "Expected: {}"
-        "".format(spec["argv"][0], sys.executable)
-    )
-    sys.exit(1)
+    spec = json.loads(raw_spec)
 
-if py_impl == "pypy" and ("ppc" in machine or "aarch64" in machine):
-    print(f"Skipping pytest on {machine} for {py_impl}")
-    sys.exit(0)
+    print("""Checking python executable: {spec["argv"][0]}""")
 
-loader = pkgutil.get_loader("ipykernel.tests")
-pytest_args = [
-    os.path.dirname(loader.path), 
-    "-vv", 
-    "--timeout=300",
-    "--asyncio-mode=auto",
-]
+    if spec["argv"][0].replace("\\", "/") != sys.executable.replace("\\", "/"):
+        print(
+            "The kernelspec seems to have the wrong prefix. \n"
+            f"""    Specfile: {spec["argv"][0]}"""
+            "\n"
+            f"""    Expected: {sys.executable}"""
+        )
+        return 1
 
-if py_impl != "pypy":
-    # coverage is very slow on pypy
-    pytest_args += [
-        "--cov=ipykernel",
-        "--cov-report=term-missing:skip-covered",
-        "--no-cov-on-fail",
+    return 0
+
+
+def build_pytest_args() -> typing.List[str]:
+    pytest_args = [
+        "--color=yes",
+        "--tb=long",
+        "-vv",
+        "--timeout=300",
+        "--asyncio-mode=auto",
     ]
 
-# TODO: investigate upstream interrupt regression in 6.5.0
-skips = ["flaky", "interrupt"]
+    if py_impl != "pypy":
+        # coverage is very slow on pypy
+        pytest_args += [
+            "--cov=ipykernel",
+            "--cov=branch",
+            "--cov-report=term-missing:skip-covered",
+            "--no-cov-on-fail",
+        ]
 
-if len(skips) == 1:
-    pytest_args += ["-k", "not {}".format(*skips)]
-else:
-    pytest_args += ["-k", "not ({})".format(" or ".join(skips))]
 
-print("Final pytest args:", pytest_args)
+    if len(test_skips) == 1:
+        # single-term parens work unexpectedly
+        pytest_args += ["-k", f"not {test_skips}"]
+    elif len(test_skips) > 1:
+        pytest_args += ["-k", f"""not ({" or ".join(test_skips)})"""]
 
-# actually run the tests
-rc = pytest.main(pytest_args)
+    return pytest_args
 
-if json.loads(os.environ.get("MIGRATING", "0").lower()):
-    print("Ignoring pytest failure due to on-going migration...")
-    sys.exit(0)
+def run_pytest():
+    if is_pypy and (is_aarch or is_ppc):
+        print(f"Skipping pytest on {machine} for {py_impl}")
+        return 0
 
-sys.exit(rc)
+    pytest_args = build_pytest_args()
+
+    print("Final pytest args:", pytest_args, flush=True)
+
+    # actually run the tests
+    rc = int(pytest.main(pytest_args))
+
+    if json.loads(os.environ.get("MIGRATING", "0").lower()):
+        print("Ignoring pytest failure due to on-going migration...")
+        return 0
+
+    return rc
+
+
+def main() -> int:
+    return check_kernel() or run_pytest()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
